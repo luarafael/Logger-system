fc5fd0a253f94d3e01a525c071002a781e9402b5
unknown <luabrandao0614@digitalcollege.internal>
2025-11-04 12:04:28 -0300

    finishing login

---
+++ src/scripts/login.js
@@ -0,0 +1,23 @@
+const username = document.querySelector("#username-login");
+const password = document.querySelector("#password-login");
+const button = document.querySelector("#button-login");
+const checkbox = document.querySelector("#checkbox-login");
+
+button.addEventListener("click", (event) => {
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+
+  if (usernameValue === " " || passwordValue === "") {
+    event.preventDefault();
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  if (usernameValue !== "luã" || passwordValue !== "123") {
+    event.preventDefault();
+    alert("Incorrect usermane or password");
+    return;
+  }
+
+  alert("Access granted !");
+});
